4b48fe3623af199aba08064a25c0affa6ba35083
Irving Juárez <irvingjuarez274@gmail.com>
2023-04-25 00:07:53 -0500

    [ADD] post method in python server with multimedia endpoint

--- main.js
+++ main.js
@@ -27,6 +27,18 @@ const showVisualFeedback = () => {
   // update UI accordingly (either good or bad feedback)
 }
 
+const getTranscription = (arrayBuffer) => () => {
+  const body = new FormData()
+  body.append("binary_data", new Blob([arrayBuffer]))
+
+  fetch("http://localhost:8080/multimedia", {
+    method: "POST",
+    body
+  })
+    .then(res => res.json())
+    .then(data => console.log)
+}
+
 const downloadFile = (evt) => {
   const file = evt.target.files[0]
   const fileSize = file.size
@@ -36,13 +48,12 @@ const downloadFile = (evt) => {
   const sizeInMegaBytes = toMegaBytes(fileSize)
 
   if (isFileValid(fileType) && isValidSize(sizeInMegaBytes)) {
-    // Continue with the program
     const reader = new FileReader()
-
     reader.readAsArrayBuffer(file)
 
-    reader.onload = postFileToManipulate
+    reader.onload = getTranscription(reader.result)
     reader.onerror = showVisualFeedback
+      
   } else {
     throw new Error("Invalid file added. Try again.")
   }
--- server/main.py
+++ server/main.py
@@ -1,6 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class Server(BaseHTTPRequestHandler):
+  def do_POST(self):
+    if self.path == "/multimedia":
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+
+      message = "Hello World! Here is a POST response"
+      self.wfile.write(bytes(message, "utf8"))
+
   def do_GET(self):
     self.send_response(200)
 
